4672c6e34a1e9aa1d9c8362569f6d069078bfb16
Hamza Zaka <39979367+hamzazaka@users.noreply.github.com>
2024-09-13 18:13:21 -0400

    adding valyes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
-import { useState } from 'react'
-import viteLogo from '/vite.svg'
 
-function App() {
 
-  return (
-    <>
-      <h1 className='text-3xl font-bold underline'>Hwllo World</h1>
-    </>
-  )
-}
-
-export default App
+const App =()=>(
+  <main className="realtive">
+    Nav
+    <section>
+      
+    </section>
+  </main>
+)

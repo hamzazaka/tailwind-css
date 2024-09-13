eaf434b2983c81c82154dd2662a29de214234b1b
Hamza Zaka <39979367+hamzazaka@users.noreply.github.com>
2024-09-13 14:47:52 -0400

    initial setup of project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import viteLogo from '/vite.svg'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-3xl font-bold underline'>Hwllo World</h1>
+    </>
+  )
+}
+
+export default App
